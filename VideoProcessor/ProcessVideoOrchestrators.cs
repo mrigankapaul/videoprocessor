7854b5451e5951f78d5647bf862bd9fa9b5bac7e
mriganka.paul <paul.mriganka@gmail.com>
2020-12-12 18:21:39 -0500

    Initial Run of Orchestrator Function Completed

---
+++ VideoProcessor/ProcessVideoOrchestrators.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+
+namespace VideoProcessor
+{
+    public static class ProcessVideoOrchestrators
+    {
+        [FunctionName("O_ProcessVideo")]
+        public static async Task<object> ProcessVideo(
+            [OrchestrationTrigger] IDurableOrchestrationContext ctx,
+            ILogger log)
+        {
+            var videoLocation = ctx.GetInput<string>();
+
+            if (!ctx.IsReplaying)
+                log.LogInformation("About to call transcode video activity");
+
+            var transcodedLocation = await
+                ctx.CallActivityAsync<string>("A_TranscodeVideo", videoLocation);
+
+            if (!ctx.IsReplaying)
+                log.LogInformation("About to call extract thumbnail");
+
+            var thumbnailLocation = await
+                ctx.CallActivityAsync<string>("A_ExtractThumbnail", transcodedLocation);
+
+            if (!ctx.IsReplaying)
+                log.LogInformation("About to call prepend intro");
+
+            var withIntroLocation = await
+                ctx.CallActivityAsync<string>("A_PrependIntro", transcodedLocation);
+
+            return new
+            {
+                Transcoded = transcodedLocation,
+                Thumbnail = thumbnailLocation,
+                WithIntro = withIntroLocation
+            };
+
+        }
+    }
+}
---
+++ VideoProcessor/ProcessVideoStarter.cs
@@ -0,0 +1,52 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace VideoProcessor
+{
+    public static class ProcessVideoStarter
+    {
+        [FunctionName("ProcessVideoStarter")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [DurableClient] IDurableOrchestrationClient starter,
+            ILogger log)
+        {
+            // parse query parameter
+            string video = req.Query["video"];
+
+
+            // Get request body
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var data = JsonConvert.DeserializeObject<RequestBody>(requestBody);
+
+            // Set name to query string or body data
+            video = video ?? data?.video;
+
+            if (video == null)
+            {
+                
+                return new BadRequestObjectResult(
+                    $"Please pass the video location the query string or in the request body"
+                    );
+                
+            }
+
+            log.LogInformation($"About to start orchestration for {video}");
+            string orchestrationId = await starter.StartNewAsync("O_ProcessVideo", video);
+            return starter.CreateCheckStatusResponse(req, orchestrationId);
+        }
+    }
+
+    public class RequestBody
+    {
+        public string video { get; set; }
+    }
+}
